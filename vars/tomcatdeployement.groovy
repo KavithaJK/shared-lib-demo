18423fd4d9907777397103c6d5a3028b0195b26a
KavithaJK <130077112+KavithaJK@users.noreply.github.com>
2023-04-06 21:27:50 +0530

    Update tomcatdeployement.groovy

--- vars/tomcatdeployement.groovy
+++ vars/tomcatdeployement.groovy
@@ -1,8 +1,8 @@
-def call (){
+def call (user,ip){
 sshagent(['tomcat']){
-       sh "scp -o StrictHostKeyChecking=no  target/*.war kavi@20.238.61.90:/opt/tomcat9/webapps"
-       sh "ssh kavi@20.238.61.90 sudo /opt/tomcat9/bin/shutdown.sh"
-       sh "ssh kavi@20.238.61.90 sudo /opt/tomcat9/bin/startup.sh"
+       sh "scp -o StrictHostKeyChecking=no  target/*.war ${user}@${ip}:/opt/tomcat9/webapps"
+       sh "ssh ${user}@${ip} sudo /opt/tomcat9/bin/shutdown.sh"
+       sh "ssh ${user}@${ip} sudo /opt/tomcat9/bin/startup.sh"
                      
      }
 }
